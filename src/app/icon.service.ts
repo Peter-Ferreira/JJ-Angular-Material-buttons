1443c4f02af90397301acf891c3bdad9eda074c1
Peter-Ferreira <42378098+Peter-Ferreira@users.noreply.github.com>
2022-12-08 08:33:54 +0200

    2 files modified, 1 added

--- src/app/icon.service.ts
+++ src/app/icon.service.ts
@@ -1,43 +1,38 @@
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import { AngularFireStorage } from '@angular/fire/storage';
-
+import { SvgIcons } from '../assets/svg-icons';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IconService {
+  svgIcons = new SvgIcons();
+
   constructor(
     private mir: MatIconRegistry,
-    private domSanitizer: DomSanitizer,
-    @Inject(AngularFireStorage) private storage: AngularFireStorage
+    private domSanitizer: DomSanitizer
   ) {
     this.registerSvgIcons();
-    const getStorage = this.storage.getStorage();
-    const spaceRef = this.storage.ref(getStorage, 'assets');  }
-
-  
-  registerSvgIcons() {
-    this.addSvgIcon('delete');
-    this.addSvgIcon('bookmark');
-    this.addSvgIcon('home');
-    this.addSvgIcon('favorite');
-    this.addSvgIcon('more_vert');
-    this.addSvgIcon('menu');
-    this.addSvgIcon('open_in_new');
-    this.addSvgIcon('exposure_plus_1');
-    this.addSvgIcon('filter_list');
-    this.addSvgIcon('bolt');
-    this.addSvgIcon('electric_bolt');
   }
 
-  addSvgIcon(iconName: string) {
-    this.mir.addSvgIcon(
+  registerSvgIcons() {
+    this.addSvgIcon('bookmark', this.svgIcons.BOOKMARK_ICON);
+    this.addSvgIcon('delete', this.svgIcons.DELETE_ICON);
+    this.addSvgIcon('electric_bolt', this.svgIcons.ELECTRIC_BOLT_ICON);
+    this.addSvgIcon('exposure_plus_1', this.svgIcons.EXPOSURE_PLUS_1_ICON);
+    this.addSvgIcon('favorite', this.svgIcons.FAVORITE_ICON);
+    this.addSvgIcon('filter_list', this.svgIcons.FILTER_LIST_ICON);
+    this.addSvgIcon('home', this.svgIcons.HOME_ICON);
+    this.addSvgIcon('menu', this.svgIcons.MENU_ICON);
+    this.addSvgIcon('more_vert', this.svgIcons.MORE_VERT_ICON);
+    this.addSvgIcon('open_in_new', this.svgIcons.OPEN_IN_NEW_ICON);
+  }
+
+  addSvgIcon(iconName: string, iconSvg) {
+    this.mir.addSvgIconLiteral(
       `${iconName}`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(
-        `assets/${iconName}_icon.svg`
-      )
+      this.domSanitizer.bypassSecurityTrustHtml(iconSvg)
     );
   }
 }
